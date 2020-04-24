02d5e85392c36e033d61bc43d61fa7feea7c6cbe
shyam <kskmail29@gmail.com>
2020-04-24 15:40:36 +1000

    updated-mars-endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/mars/shyam', function (req, res) {
+  res.send('Hello Kumar!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
